ff35db83d7c717a4ddab01f5f7c96b180e6894c4
paulbowler <paul_bowler@yahoo.com>
2013-10-15 22:09:34 +0100

    Added start of a solution for Q5

--- src/euler/5.clj
+++ src/euler/5.clj
@@ -1,2 +1,13 @@
 (ns euler.core)
 
+(defn factors [n]
+  (sorted-set (filter #(= 0 (rem n %)) (range 1 n))))
+
+(factors 2520)
+
+(reduce * (range 1 21))
+
+(* 2 3 5 7 11 13 17 19)
+
+(* 20 19 17 13 11 7 5 3 2)
+
