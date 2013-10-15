08764c07c4f4c16915af71b6079cb4d7886024c8
paulbowler <paul_bowler@yahoo.com>
2013-10-15 15:26:36 +0100

    Added improved solution to Q2

--- src/euler/2.clj
+++ src/euler/2.clj
@@ -1,3 +1,9 @@
 (ns euler.core)
 
 (reduce + (filter even? (take-while #(< % 4000000) (map first (iterate (fn [[a b]] [b (+ a b)]) [1 2])))))
+
+(->> (iterate (fn [[a b]] [b (+ a b)]) [1 2])
+     (map first)
+     (take-while #(< % 4000000))
+     (filter even?)
+     (reduce +))
