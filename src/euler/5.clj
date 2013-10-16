9e80d75793c13bada4ae58715f8fecbf682a765e
paulbowler <paul_bowler@yahoo.com>
2013-10-16 13:41:57 +0100

    Updated comments to solution to Q5

--- src/euler/5.clj
+++ src/euler/5.clj
@@ -1,5 +1,11 @@
 (ns euler.core)
 
+; After trying (and failing) to find a solution using brute-force factorisations I found
+; a simpler mathematical solution using greatest common divisor here:
+;
+;   http://en.wikipedia.org/wiki/Least_common_multiple#Reduction_by_the_greatest_common_divisor
+;
+; Essentially: lcm(a,b) = (a*b)/gcd(a,b) which can then be reduced over range 1..20
 
 (defn gcd [a b] (if (zero? b) a (recur b (mod a b))))
 (defn lcm [a b] (/ (* a b) (gcd a b)))
