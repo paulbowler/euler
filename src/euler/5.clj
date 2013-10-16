19845d4080c9f3f561125fee28953842ca53b845
paulbowler <paul_bowler@yahoo.com>
2013-10-16 13:26:54 +0100

    Improved solution for Q5

--- src/euler/5.clj
+++ src/euler/5.clj
@@ -1,13 +1,7 @@
 (ns euler.core)
 
-(defn factors [n]
-  (sorted-set (filter #(= 0 (rem n %)) (range 1 n))))
 
-(factors 2520)
+(defn gcd [a b] (if (zero? b) a (recur b (mod a b))))
+(defn lcm [a b] (/ (* a b) (gcd a b)))
 
-(reduce * (range 1 21))
-
-(* 2 3 5 7 11 13 17 19)
-
-(* 20 19 17 13 11 7 5 3 2)
-
+(reduce #(lcm %1 %2) (range 1 21))
