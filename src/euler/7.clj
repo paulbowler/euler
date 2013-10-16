972c1d1e167f5a16db2d454c5e2d8ffdee8ef82c
paulbowler <paul_bowler@yahoo.com>
2013-10-16 14:09:05 +0100

    Added solution to Q7

---
+++ src/euler/7.clj
@@ -0,0 +1,20 @@
+(ns euler.core)
+
+; We can reuse the prime factor code from Q3 for an (inefficient) solution
+
+(defn largest-prime-factor
+  ([n] (largest-prime-factor n 2))
+  ([n d]
+   (if (> d n)
+     (dec d)
+     (recur
+      (#(if (zero? (rem % d))
+          (recur (/ % d))
+          %)
+        n)
+      (inc d)))))
+
+(defn prime? [n]
+  (= n (largest-prime-factor n)))
+
+(nth (filter prime? (iterate inc 1)) 10001)
